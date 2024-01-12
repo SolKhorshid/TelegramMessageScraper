9989c2fb3beed36b225ca8e6bc850172e6851688
SolKhorshid <156417900+SolKhorshid@users.noreply.github.com>
2024-01-12 16:24:59 +0100

    Create api_info.py

---
+++ api_info.py
@@ -0,0 +1,7 @@
+# Set up your API credentials
+api_id =        # integer
+api_hash = ''       # string
+
+# Your personal phone number linked to your Telegram account
+phone_number = ''       # string. Remember + country code
+username = ''       # string. Your username
